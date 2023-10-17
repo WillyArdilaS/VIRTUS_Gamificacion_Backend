988cb41730833fd3647d5b9476934fade29ad645
andres.jimenez <andres.jimenez@ingenian.com>
2023-10-16 19:38:42 -0500

    Vencimiento actividad

--- controllers/actividadController.js
+++ controllers/actividadController.js
@@ -9,9 +9,7 @@ const actividadGET = async (req = request, res = response) => {
         Actividad.countDocuments(query),
         Actividad.find(query)]);
 
-    // Verificar y actualizar la disponibilidad
     await actualizarDisponibilidadActividades(actividadesBD);
-
 
     res.status(200).json({
         msg: 'Get API',
@@ -24,7 +22,7 @@ const actividadPOST = async (req = request, res = response) => {
     const {fechaVencimiento, recompensa, castigo, descripcion, dificultad, claseFK} = req.body;
 
     const actividad = new Actividad({fechaVencimiento, recompensa, castigo, descripcion, dificultad, claseFK});
-
+    console.log(actividad)
     usuarioAutenticado = req.usuario;
 
     //Guardamos en BD
@@ -38,12 +36,8 @@ const actividadPOST = async (req = request, res = response) => {
 }
 
 const actualizarDisponibilidadActividades = async (actividades) => {
-    const fechaActual = new Date();
-
-    // Iterar sobre las actividades y actualizar la disponibilidad según la fecha de vencimiento
     for (const actividad of actividades) {
-        if (actividad.disponible && actividad.fechaVencimiento <= fechaActual) {
-            // La fecha de vencimiento ha pasado, actualizar la disponibilidad a false
+        if (actividad.disponible && new Date(actividad.fechaVencimiento) <= new Date()) {
             await Actividad.findByIdAndUpdate(actividad._id, { disponible: false });
         }
     }
